0e782493c2695ed119cf569ddc2cf73b8a43c8ca
Mullerino1 <martinemu03@gmail.com>
2024-03-14 10:30:55 +0100

    LayoutAdded
    Layout with linking and children added

---
+++ src/Components/Layout.jsx
@@ -0,0 +1,41 @@
+/*import { useState } from 'react'
+
+import ResourceList from './ResourceList'*/
+//import Nav from './Nav'
+import {Outlet, Link } from "react-router-dom";
+
+export default function Layout({children}){
+  return (
+    <>
+    <div id="container">
+    
+  <nav>
+      <ul>
+        <li>
+          <Link to="/javascript">Javascript</Link>
+        </li>
+        <li>
+          <Link to="/css">CSS</Link>
+        </li>
+        <li>
+          <Link to="/react">React</Link>
+        </li>
+        <li>
+          <Link to="/sanity">Sanity</Link>
+        </li>
+        <li>
+          <Link to="/html">HTML</Link>
+        </li>
+      </ul>
+    </nav>
+    
+    <main>
+        {children}
+        <Outlet />
+    </main>
+    
+    </div>
+   
+    </>
+  )
+}
