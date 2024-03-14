9d767a405d6f1ae58c5769c8ae0432f412775bbc
Mullerino1 <martinemu03@gmail.com>
2024-03-14 11:31:48 +0100

    NameChanges

--- src/Components/Sanity.jsx
+++ src/Components/Sanity.jsx
@@ -1,15 +1,15 @@
 import { resources } from "../assets/ressurser"
 
-export default function Sanity () {
+export default function HeadlessCMS () {
 
-  const filterSanity = resources.filter(item => item.category === "headless-cms")
+  const filterCMS = resources.filter(item => item.category === "headless-cms")
 
     return (
         <div>
-            <h2 id="text">Sanity</h2>
+            <h2 id="text">Headless CMS</h2>
 
             <ul>
-              {filterSanity.map((item, index) => (
+              {filterCMS.map((item, index) => (
               <li key={index}>
                 <a href={item.url}>{item.title}</a>
             </li>
